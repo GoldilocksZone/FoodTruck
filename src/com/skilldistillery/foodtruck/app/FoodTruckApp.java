97eb8cc5dc3e01994dec8ea5e9130d6a3a6211e8
GoldilocksZone <jmarulli@gmail.com>
2022-06-25 20:37:53 -0400

    Added methods to both classes and developed one JUnit test.

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -2,20 +2,70 @@ package com.skilldistillery.foodtruck.app;
 
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
 /*
  * 	Directions:
  * 
  * 	TODO	(complete)	You will create a separate class with a main method that starts the program.
- * 	TODO	It will have an array to store up to five FoodTruck objects.
+ * 	TODO	(complete)	It will have an array to store up to five FoodTruck objects.
  * 	TODO	The main method of this class is the only static method in the entire project.
  * 
  */
 
 public class FoodTruckApp {
-	private FoodTruck[] foodTrucks;
-	
+	private Scanner scanner = new Scanner(System.in);
+	private FoodTruck[] foodTrucks; // TODO Should this go inside the main?
+	private int remainingReviews = 5;
+
 	public static void main(String... args) {
-		
+		printSplashScreen();
+		getUserReview();
+	}
+
+	private void printSplashScreen() {
+		int remainingReviews; // no default value because local variable
+		System.out.println("**********************");
+		System.out.println("*       Truckr       *");
+		System.out.println("* Food Truck Reviews *");
+		System.out.println("**********************");
+		System.out.println();
+		if (foodTrucks != null) {
+			remainingReviews = 5 - foodTrucks.length;
+		}
+		System.out.println(remainingReviews + " of 5 available reviews remaining.");
+		System.out.println();
+		System.out.println("Please leave your food truck review below, and don't forget to follow us on Instagram! @FoodTruckApp");
+		System.out.println();
 	}
 	
+	private void printReviewTitle() {
+		System.out.print(" Review - Food Truck #" + (remainingReviews - 4));
+	}
+	
+	private void printReviewTitle(int index) {
+		System.out.print(" Review - Food Truck #" + foodTrucks[index]);
+	}
+	
+	private void getUserReview() {
+		printReviewTitle();
+		getNameFromUser();
+	}
+	
+	private String getNameFromUser() {
+		System.out.println("Name: ('quit' to quit) ");
+		return scanner.nextLine();
+	}
+
+	private String getCategoryFromUser() {
+		System.out.println("Category: ");
+		return scanner.nextLine();
+	}
+
+	private int getRatingFromUser() {
+		System.out.println("Rating: (1-5) ");
+		return scanner.nextLine();
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,8 +1,10 @@
 package com.skilldistillery.foodtruck.entities;
 
+import java.util.UUID;
 
 /*
  * 	Directions:
+ * 	TODO (complete)
  * 	You will define a FoodTruck class with fields for:
  * 	-	a unique numeric id,
  * 	-	a name ("TacoRific", "Mediterranean Medic", etc.),
@@ -12,30 +14,54 @@ package com.skilldistillery.foodtruck.entities;
  */
 
 public class FoodTruck {
-	private int id;
-	private String name, category;
-	private double rating;
+	private UUID id = null;
+	private String name = null, category = null;
+	private int rating = -1;
 	
-	{
-		
+	public FoodTruck () {
+		this.id = UUID.randomUUID();
+		this.name = "unknown";
+		this.category = "unknown";
 	}
 	
-	FoodTruck () {
-		
+	public FoodTruck (String name) {
+		this();
+		this.name = name;
 	}
 	
-	FoodTruck (String name) {
-		
+	public FoodTruck (String name, String category) {
+		this(name);
+		this.category = category;
 	}
 	
-	FoodTruck (String name, String category) {
-		
+	public FoodTruck (String name, String category, int rating) {
+		this(name, category);
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return new String(this.name);
 	}
 	
-	FoodTruck (String name, String category, double rating) {
-		
+	public void setName(String name) {
+		this.name = name;
 	}
 	
+	public String getCategory() {
+		return new String(this.category);
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public int getRating() {
+		return this.rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
 	
 	
 }
